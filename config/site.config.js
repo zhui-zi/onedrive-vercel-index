4906b066f07b450d5d2262c4d85d8b469afae8c5
坠子 <69102733+zhui-zi@users.noreply.github.com>
2022-05-24 09:31:46 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,7 +49,7 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'globe',
+      name: 'My Homepage',
       link: 'https://zhuizi.me',
     },
   ],
